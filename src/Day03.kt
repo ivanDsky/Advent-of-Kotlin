fc8f42f832b87845f01996949ba1093411d77669
Ivan Dobrovolskyi <ivan.dobrovolskyi.se@gmail.com>
2024-12-03 18:17:29 -0500

    Refactor Day03

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,34 +1,42 @@
 fun main() {
-    fun parseInput(input: String): List<List<Long>> {
-        val regex = Regex("""mul\(\d{1,3},\d{1,3}\)""")
-        val digit = Regex("""\d{1,3}""")
-        return regex.findAll(input).map { digit.findAll(it.value).map { it.value.toLong() }.toList() }.toList()
-    }
-
-    fun parseInput2(input: String): List<List<Long>> {
-        val doDont = Regex("""do\(\).*?don't\(\)""")
-
-        return doDont.findAll(input).map { parseInput(it.value) }.flatten().toList()
+    fun String.readDigits(): List<Pair<Long, Long>> {
+        val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
+        return regex.findAll(this).map { result ->
+            val (firstNumber, secondNumber) = result.groupValues.drop(1).map { it.toLong() }
+            firstNumber to secondNumber
+        }.toList()
     }
 
     fun part1(input: String): Long {
-        val res = parseInput(input)
-        return res.sumOf { (a, b) -> a * b }
+        val digits = input.readDigits()
+        return digits.sumOf { (a, b) -> a * b }
+    }
+
+
+    fun String.readAllowedDigits(): List<Pair<Long, Long>> {
+        val modifiedInput = "do()${this}don't()"
+        val doDont = Regex("""do\(\).*?don't\(\)""")
+
+        return doDont
+            .findAll(modifiedInput)
+            .map { it.value.readDigits() }
+            .flatten()
+            .toList()
     }
 
     fun part2(input: String): Long {
-        val res = parseInput2(input)
-        return res.sumOf { (a, b) -> a * b }
+        val digits = input.readAllowedDigits()
+        return digits.sumOf { (a, b) -> a * b }
     }
 
     // Or read a large test input from the `src/Day03_test.txt` file:
-    val testInput = readInput("Day03_test").first()
+    val testInput = readRawInput("Day03_test")
 
     check(part1(testInput) == 161L)
     check(part2(testInput) == 48L)
 
     // Read the input from the `src/Day03.txt` file.
-    val input = readInput("Day03").joinToString("")
+    val input = readRawInput("Day03")
     part1(input).println()
     part2(input).println()
 }
--- src/Utils.kt
+++ src/Utils.kt
@@ -6,6 +6,11 @@ import kotlin.io.path.readText
 /**
  * Reads lines from the given input txt file.
  */
+fun readRawInput(name: String) = Path("src/$name.txt").readText().trim()
+
+/**
+ * Reads lines from the given input txt file.
+ */
 fun readInput(name: String) = Path("src/$name.txt").readText().trim().lines()
 
 /**
